8ccd27219d38260a6ea4f82700b965480f120fa3
neijunior1995 <neijunior1995@outlook.com>
2023-01-06 09:24:51 -0400

    primeiros passos implementação da metologia crítico autor contínuo

---
+++ deep-Q/Utils/actor_critic.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class PolicyActor(nn.Module):
+    """
+    Impementação utilizada pelo autor contínuo
+    """
+    def __init__(self):
+        super(PolicyActor,self).__init__(input_shape):
+        self.input_shape = input_shape
+
+        self.layer1 = nn.Linear(input_shape, 512)
+        self.layer2 = nn.Linear(512, 256)
+        self.layer3 = nn.Linear(256, 64)
+        self.output_layer = nn.Tanh()
+    def forward(self, input):
+        x = F.relu(self.layer1(input))
+        x = F.relu(self.layer2(x))
+        x = F.relu(self.layer3(x))
+
+class Actor(nn.Module):
+    """
+    Implementação da classe do ator utilizado no deep-Qlearning crítico ator.
+    """
+    def __init__(self):
+        self.input_shape = input_shape
+        self.action_space = action_space
+        self.Model = PolicyActor(input_shape)
+
+
